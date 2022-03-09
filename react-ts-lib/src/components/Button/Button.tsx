60c27ac01d945331d08602e54f493e1abacf8dac
Andrew McCallum <amccallum89@gmail.com>
2022-03-09 17:00:05 +0000

    added back useState

--- react-ts-lib/src/components/Button/Button.tsx
+++ react-ts-lib/src/components/Button/Button.tsx
@@ -1,14 +1,18 @@
-import React, { FC } from 'react';
+import React, { FC, useEffect } from 'react';
 
-import './Button.css';
 import { ButtonProps } from './Button.types';
 
 const Button: FC<ButtonProps>  = ({ type, text, onClick }) => {
-    return (
-        <button type="button" className={`Button Button-${type}`} onClick={onClick}>
-            {text}
-        </button>
-    )
+
+  useEffect(() => {
+    console.log("working")
+  }, [])
+
+  return (
+    <button type="button" className={`Button Button-${type}`} onClick={onClick}>
+      {text}
+    </button>
+  )
 };
 
 export default Button;
